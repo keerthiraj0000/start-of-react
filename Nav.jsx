26de3bd6a99090ddfd3663bca0bfd7bf645848ea
keerthiraj0000 <120468925+keerthiraj0000@users.noreply.github.com>
2023-03-07 18:18:44 +0530

    Add files via upload

---
+++ Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import './nav.css'
+export default function Nav(){
+    return( <nav>
+ 
+      <a href="NETFLIX">NETFLIX</a>
+      <ul>
+<li>
+    HOME
+</li>
+<li>
+
+    ABOUT
+</li>
+
+
+      </ul>
+
+
+
+
+
+    </nav>
+
+          
+
+
+
+
+
+
+    )
+}
