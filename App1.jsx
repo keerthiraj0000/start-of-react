c9bd8d5d25f6a6f9fd3cb98a8bf62cd63ea949cc
keerthiraj0000 <120468925+keerthiraj0000@users.noreply.github.com>
2023-03-08 19:56:37 +0530

    Add files via upload

---
+++ App1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ import photo from './photo.jpg' ;
+import C1 from "./C1";
+const App1=()=>{
+    return(<>
+    <img src="https://tse4.mm.bing.net/th?id=OIP.zv0l_98rayvrcaf5kxnJ_gHaD2&pid=Api&P=0" alt="" />
+     <C1 data={photo}/>
+      </>
+    )
+
+}
+export default App1
---
+++ C1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import P from './P'
+
+ const C1=(props)=>{
+   console.log(props);
+   
+   return(<>
+    <img src={props.data} alt="" />
+    </>)
+}
+export default C1
