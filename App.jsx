6b1a0d9be1351df249151615ebe3b8fc29a5a664
keerthiraj0000 <120468925+keerthiraj0000@users.noreply.github.com>
2023-03-08 19:41:24 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-// import photo from './photo' ;
+// import photo from './photo.jpg' ;
+import C from "./C";
 const App=()=>{
     return(<>
-        <img src="https://tse1.mm.bing.net/th?id=OIP.x4s3puUdx55RFefTsJMCjQHaEK&pid=Api&rs=1&c=1&qlt=95&w=215&h=120" alt="bike" />
-        {/* <img src={photo} alt="" /> */}
+        <C data={{username:"rajaaaa",
+                 password:123}}/>
     </>
     )
 
--- C.jsx
+++ C.jsx
@@ -2,9 +2,10 @@ import React from "react";
 // import P from './P'
 
 const C=(props)=>{
-    return(<>
-  <h1>{props.child}</h1>
-  {/* <P parent="appa"/> */}
+   console.log(props);
+   
+   return(<>
+     <h1>{props.data.password}</h1>
     </>)
 }
 export default C
